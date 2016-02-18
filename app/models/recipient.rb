d9e1769168e19f3e1057e8100abd43d55ffe574f
Neal Friedman <nealiof1000@gmail.com>
2016-02-18 14:03:38 -0700

    reset some devise stuff that I removed and broke the server with

--- app/models/recipient.rb
+++ app/models/recipient.rb
@@ -1,4 +1,8 @@
 class Recipient < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
   belongs_to :recipient_map
   validates_presence_of :email_address, :id, :pub_key, :message => "Must have email address, id, and public key"
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  devise_for :recipients
   get 'home/index'
 
   devise_for :recipients
