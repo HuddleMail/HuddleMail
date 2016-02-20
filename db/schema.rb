92425e24c2ec03c86159c9bc31492e080e302014
Neal Friedman <nealiof1000@gmail.com>
2016-02-20 16:02:22 -0700

    Reset database and updates the schema
    Added some settings to development

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,32 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160217004446) do
+ActiveRecord::Schema.define(version: 20160220204129) do
 
-  create_table "distgroups", force: :cascade do |t|
-    t.integer "dg_id",   null: false
-    t.string  "sender",  null: false
-    t.text    "pub_key", null: false
+  create_table "dist_group_recipient_maps", force: :cascade do |t|
+    t.integer "dist_group_id"
+    t.integer "recipient_id"
+  end
+
+  add_index "dist_group_recipient_maps", ["dist_group_id"], name: "index_dist_group_recipient_maps_on_dist_group_id"
+  add_index "dist_group_recipient_maps", ["recipient_id"], name: "index_dist_group_recipient_maps_on_recipient_id"
+
+  create_table "dist_groups", force: :cascade do |t|
+    t.string "email",   null: false
+    t.text   "pub_key", null: false
+  end
+
+  create_table "profile_dist_group_maps", force: :cascade do |t|
+    t.integer "profile_id"
+    t.integer "dist_group_id"
+  end
+
+  add_index "profile_dist_group_maps", ["dist_group_id"], name: "index_profile_dist_group_maps_on_dist_group_id"
+  add_index "profile_dist_group_maps", ["profile_id"], name: "index_profile_dist_group_maps_on_profile_id"
+
+  create_table "profiles", force: :cascade do |t|
+    t.string "email",    limit: 254, null: false
+    t.string "password",             null: false
   end
 
   create_table "recipients", force: :cascade do |t|
