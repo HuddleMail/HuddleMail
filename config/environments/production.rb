5e209ca339ddb850f6e7dfb93da6d4497247e84f
Neal Friedman <nealiof1000@gmail.com>
2016-02-20 23:45:05 -0700

    fixes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,27 +76,4 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-
-
-
-
-  ###### !!!!!! MUST SET THIS UP PROPERLY FOR PRODUCTION !!!!!!!!!!! ###########
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_options = {from: 'do-not-reply@huddlemail.xyz'}
-
-
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host: 'sitepoint-devise.huddlemail.xyz' }
-
-  ActionMailer::Base.smtp_settings = {
-      :address        => 'smtp.sendgrid.net',
-      :port           => '587',
-      :authentication => :plain,
-      :user_name      => ENV['SENDGRID_USERNAME'],
-      :password       => ENV['SENDGRID_PASSWORD'],
-      :domain         => 'huddlemail.xyz',
-      :enable_starttls_auto => true
-  }
-  #######  !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!   #################
 end
