5304c7735b90aad58122dcb2225da325ee84b7cd
Neal Friedman <nealiof1000@gmail.com>
2016-02-20 22:17:48 -0700

    Added some mailer configurations

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,8 +39,18 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
-  config.action_mailer.perform_deliveries = true
+
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+
+  config.action_mailer.delivery_method = :sendmail
+  # Defaults to:
+  # config.action_mailer.sendmail_settings = {
+  #   location: '/usr/sbin/sendmail',
+  #   arguments: '-i -t'
+  # }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_options = {from: 'do-not-reply@huddlemail.xyz'}
 
 
 end
