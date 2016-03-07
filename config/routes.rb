90893ecbc69bd5d8f04a33d3923057c0ff39ddf4
Neal Friedman <nealiof1000@gmail.com>
2016-03-07 16:04:20 -0700

    Added devise scopes. and changed home routes to pages (because home controller will not be used)

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,26 @@
 Rails.application.routes.draw do
-  root 'home#index'
-  get 'home/index'
-  get 'home/register'
-  get 'home/login'
-  get 'home/register'
-  post 'home/create'
-  post 'home/login'
+  devise_for :users, controllers: { sessions: "users/sessions" }
+  root 'pages#show', page: 'index'
+
+  devise_scope :user do
+    get "/users/registrations/new" => "users/registrations"
+  end
+
+  devise_scope :user do
+    get "/users/sessions/new" => "users/sessions"
+  end
+
+
+  Rails.application.routes.draw do
+    get "/pages/:page" => "pages#show"
+  end
+
+  get 'pages/index'
+  get 'pages/register'
+  get 'pages/login'
+  get 'pages/register'
+  post 'pages/create'
+  post 'pages/login'
   get 'home/success'
+  get 'users/registrations/new'
 end
