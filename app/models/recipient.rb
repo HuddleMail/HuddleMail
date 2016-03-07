1759ae0877fd4b5d831c3b917205cddddfaf75c1
Chad-D <chaddierking@gmail.com>
2016-03-07 16:30:33 -0700

    All rows in recipient table must have a unique email address and public key combination.

--- app/models/recipient.rb
+++ app/models/recipient.rb
@@ -1,4 +1,5 @@
 class Recipient < ActiveRecord::Base
   validates :email, presence: true, uniqueness: true
   validates :pub_key, presence: true
+  validates :uniqueness => {:scope => [:email, :pub_key]}
 end
