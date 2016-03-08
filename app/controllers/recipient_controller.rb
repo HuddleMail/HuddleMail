95d962ddcb50be4fe6397048fa57604b237d49ae
Neal Friedman <nealiof1000@gmail.com>
2016-03-08 07:36:13 -0700

    Added index function

--- app/controllers/recipient_controller.rb
+++ app/controllers/recipient_controller.rb
@@ -1,10 +1,12 @@
 class RecipientController < ApplicationController
   before_action :authenticate_user!
 
+  def index
+    @recipient = Recipient.all
+  end
+
   def new
     @recipient = Recipient.new
   end
-
-
 
 end
