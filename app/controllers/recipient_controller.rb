80e5edaf6d7061949a86e07aae41361862f6785b
Neal Friedman <nealiof1000@gmail.com>
2016-03-08 20:32:06 -0700

    added params to controllers

--- app/controllers/recipient_controller.rb
+++ app/controllers/recipient_controller.rb
@@ -9,4 +9,15 @@ class RecipientController < ApplicationController
     @recipient = Recipient.new
   end
 
+  private
+  # Use callbacks to share common setup or constraints between actions.
+  def set_recipients
+    @recipient = Recipient.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def recipient_params
+    params.require(:recipient).permit(:email, users_attributes: [ :email, :pub_key ])
+  end
+
 end
--- app/models/dist_group.rb
+++ app/models/dist_group.rb
@@ -1,4 +1,6 @@
 class DistGroup < ActiveRecord::Base
   validates :email, presence: true, uniqueness: true
   validates :pub_key, presence: true
+
+
 end
