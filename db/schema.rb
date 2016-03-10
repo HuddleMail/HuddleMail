e240d14ccdac24dba019afb5220f16bf5c6431a1
Neal Friedman <nealiof1000@gmail.com>
2016-03-09 18:11:59 -0700

    autogenerated

--- db/schema.rb
+++ db/schema.rb
@@ -11,33 +11,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160307171855) do
-
-  create_table "dist_group_recipient_maps", force: :cascade do |t|
-    t.integer "dist_group_id"
-    t.integer "recipient_id"
-  end
-
-  add_index "dist_group_recipient_maps", ["dist_group_id"], name: "index_dist_group_recipient_maps_on_dist_group_id"
-  add_index "dist_group_recipient_maps", ["recipient_id"], name: "index_dist_group_recipient_maps_on_recipient_id"
+ActiveRecord::Schema.define(version: 20160310010655) do
 
   create_table "dist_groups", force: :cascade do |t|
-    t.string "email",   null: false
-    t.text   "pub_key", null: false
+    t.string   "dist_name"
+    t.text     "pub_key"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table "profile_dist_group_maps", force: :cascade do |t|
-    t.integer "profile_id"
-    t.integer "dist_group_id"
-  end
-
-  add_index "profile_dist_group_maps", ["dist_group_id"], name: "index_profile_dist_group_maps_on_dist_group_id"
-  add_index "profile_dist_group_maps", ["profile_id"], name: "index_profile_dist_group_maps_on_profile_id"
+  add_index "dist_groups", ["user_id"], name: "index_dist_groups_on_user_id"
 
   create_table "recipients", force: :cascade do |t|
-    t.string "email",   limit: 254, null: false
-    t.text   "pub_key",             null: false
+    t.string   "email"
+    t.text     "pub_key"
+    t.integer  "dist_group_id"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
   end
+
+  add_index "recipients", ["dist_group_id"], name: "index_recipients_on_dist_group_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
