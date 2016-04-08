0057cb2f8721b003de31396048702e533af8703c
Neal <neal@huddlemail.xyz>
2016-04-08 00:34:49 -0600

    deleted old gems

--- scripts/encrypter.rb
+++ scripts/encrypter.rb
@@ -1,18 +1,19 @@
-#!/bin/ruby
+#!/usr/bin/ruby
 
 ## Read in Encrypted Message from STDIN
-incoming = ARGF.read
-puts incoming
+incoming = $stdin.read
+#puts incoming
 
-`echo "#{incoming}" > output`
+f = File.open('/tmp/neal','w')
+f.puts incoming
 
 ## Encrypt the message for Recipient
 encrypted = `echo "#{incoming}" | gpg --no-batch -a -e -r "nealio"`
-puts encrypted
+#puts encrypted
 
 ## Decrypt the Incoming Group Message
 decrypted = `echo "#{encrypted}" | gpg -a --no-batch -d`
-puts decrypted
+#puts decrypted
 
 ## Email Encrypted Message to Recipient
 `echo "#{encrypted}" | mail -s "ENCRYPTED" nealiof1000@gmail.com`
