59831e5ecbb152adbc2f6704e64228b2d2ad0395
Sann <sannvoun@gmail.com>
2016-04-09 13:11:02 -0600

    home link redirect to dist_groups/index. Dist_groups/index shows only current users DG. Link to edit DG is gone.

--- app/controllers/dist_groups_controller.rb
+++ app/controllers/dist_groups_controller.rb
@@ -14,8 +14,8 @@ class DistGroupsController < ApplicationController
   end
 
 	#GET /dist_groups/user_index
-	def user_index
-    #@dist_groups = DistGroup.all
+	def all
+    @dist_groups = DistGroup.all
 		@current_id = current_user.id
     @user_groups = DistGroup.where(:user_id => @current_id)
 	end
@@ -95,9 +95,9 @@ class DistGroupsController < ApplicationController
 	  gpg = OpenPGP::Engine::GnuPG.new(:homedir => '~/.gnupg')
 	  key_id = gpg.gen_key({
 		  :key_type      => 'RSA',
-		  :key_length    => 1024,
+		  :key_length    => 4096,
 		  :subkey_type   => 'RSA',
-		  :subkey_length => 1024,
+		  :subkey_length => 4096,
 		  :name          => @dname,
 		  :comment       => nil,
 		  #:email         => '',
