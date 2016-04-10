92b19fa592b13361f93253bc8d4a75e2b1a58d8e
Neal Friedman <nealiof1000@gmail.com>
2016-04-09 20:59:25 -0600

    made it so if user is logged in, redirect to distgroup/all else go to index

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,14 +1,8 @@
 class PagesController < ApplicationController
-  def show
-    if valid_page?
-      render template: "pages/#{params[:page]}"
-    else
-      render file: "public/404.html", status: :not_found
+  def index
+    if user_signed_in?
+      redirect_to dist_groups_all_path
     end
   end
-
-  private
-  def valid_page?
-    File.exist?(Pathname.new(Rails.root + "app/views/pages/#{params[:page]}.html.erb"))
-  end
 end
+
