24ec567e861520de6f61af33996d609387db3d4c
Neal Friedman <nealiof1000@gmail.com>
2016-04-11 11:49:04 -0600

    removed incoming output

--- scripts/mailer.rb
+++ scripts/mailer.rb
@@ -46,7 +46,7 @@ recipients.each do |recipient|
  recipkeys = File.open('/tmp/recipient_key.asc', 'w+')
  recipkeys.puts recipient.pub_key
 
- # recipkeys.close
+ recipkeys.close
 
  ## import the recipients key
   `gpg  --import /tmp/recipient_key.asc`
